d4713dd4f980712f5a69fef321f33839bc167f64
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2024-04-13 21:09:53 -0700

    python can trigger exercise feature

--- sleepy.py
+++ sleepy.py
@@ -5,13 +5,17 @@ import serial.tools.list_ports
 arduino_ports = [
     p.device
     for p in serial.tools.list_ports.comports()
-    if 'Arduino' in p.description  # may need tweaking to match new arduinos
+    if 'USB' in p.description  # may need tweaking to match new Arduinos
 ]
 if not arduino_ports:
     raise IOError("No Arduino found")
 if len(arduino_ports) > 1:
     warnings.warn('Multiple Arduinos found - using the first')
 
-ser = serial.Serial(arduino_ports[0])
+ser = serial.Serial(arduino_ports[0], 9600)
 
-ser.write("Sleepy")
+# Encode the string before writing it to the serial port
+i = 0
+while (i<100):
+    ser.write(b'Sleepy\n')
+    i+=1
--- testEMG/testEMG.ino
+++ testEMG/testEMG.ino
@@ -2,7 +2,9 @@ int emgVal = 0;
 int count = 0;
 const int emgPin = A0;
 const int buzzPin = 8;
-int buzzFlag = 1;
+const int thres = 600;
+const int time = 500;
+int buzzFlag = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -11,18 +13,24 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  String input = Serial.readString();
-  if(input == "Sleepy"){
-    buzzFlag = 1;
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n'); // Read until newline character
+    if (input == "Sleepy") {
+      buzzFlag = 1;
+      count = 0;
+    }
   }
+
   Serial.println(analogRead(emgPin));
   if (buzzFlag == 1) {
     tone(buzzPin, 1000);
   }
-  if (analogRead(emgPin) > 150) {
+  if (analogRead(emgPin) > thres) {
     count++;
   }
-  if (count > 300)
+  if (count > time) {
     buzzFlag = 0;
     noTone(buzzPin);
+  }
+  delay(10);
 }
